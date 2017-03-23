4761c7178ed45f3a494f10c4aef06b900e5dcc1e
malik-aliyev-94 <malik.aliyev.94@gmail.com>
2017-03-23 22:38:10 +0400

    002 Watch webpack config "yarn nodemon"

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,19 @@
 const { resolve } = require('path');
-const glob = require('glob');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const DashboardPlugin = require('webpack-dashboard/plugin');
-const autoprefixer = require('autoprefixer')({
-        browsers: ['last 30 versions'],
-    });
-
-const entriesObj = {
-  hmr: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server'
-  ],
-  files: glob.sync("./src/entries/*.js")
-};
-
-const entry = entriesObj['hmr'].concat(entriesObj['files']);
-entry.map(function(path, index){
-    entry[index] = path.replace(/\.\/src/, ".")
-});
 
 module.exports = {
   context: resolve(__dirname, 'src'),
-  entry: entry,
+  // entry: entry,
+  entry: {
+    'vendor': ['react-hot-loader/patch', 'webpack-dev-server/client?http://localhost:8080', 'webpack/hot/only-dev-server'],
+    'main': ['./entries/index.js', './entries/script.js', './entries/test.js']
+  },
   output: {
-    filename: 'bundle.js',
-    // the output bundle
+    filename: 'scripts/[name].bundle.js',
     path: resolve(__dirname, 'dist'),
     publicPath: '/'
-    // necessary for HMR to know where to load the hot update chunks
   },
 
   devtool: 'inline-source-map',
@@ -56,23 +40,23 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader?modules',
-          'postcss-loader',
-        ],
-      },
-    ],
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: 'css-loader?importLoaders=1!postcss-loader'
+        })
+      }
+    ]
   },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     // enable HMR globally
-
     new webpack.NamedModulesPlugin(),
     // prints more readable module names in the browser console on HMR updates
-
     new DashboardPlugin(),
-    new webpack.LoaderOptionsPlugin({ options: { postcss: [ autoprefixer ] } })
+    new ExtractTextPlugin({
+      filename: 'styles/[name].bundle.css',
+      allChunks: true
+    })
   ],
 };
